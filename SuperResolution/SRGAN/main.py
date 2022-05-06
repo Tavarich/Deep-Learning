37473156874c410b1246fb45777841d9db7d7044
Tavarich <61419255+Tavarich@users.noreply.github.com>
2022-05-06 10:43:26 +0800

    Update main.py

--- SuperResolution/SRGAN/main.py
+++ SuperResolution/SRGAN/main.py
@@ -26,17 +26,17 @@ def predict(model, img, scaling_size=2, mode='gpu'):
     sr_tensor = torch.clamp(sr_tensor, 0, 1)  # 裁剪超出0——1范围的像素值
     sr_img = transforms.ToPILImage()(sr_tensor)
 
-    sr_img.save('/root/CV/SuperResolution/srgan/result/srgan.jpg')
+    sr_img.save('/root/CV/SuperResolution/srgan/result/srgan.jpg')  # SRGAN生成超分图位置，可自行修改
 
     bi_img = img.resize((int(img.width * scaling_size), int(img.height * scaling_size)), Image.BICUBIC)
-    bi_img.save('/root/CV/SuperResolution/srgan/result/bicubic.jpg')
+    bi_img.save('/root/CV/SuperResolution/srgan/result/bicubic.jpg') # Bicubic插值生成超分图位置，可自行修改
 
 
 def main():
-    img_path = "/root/autodl-tmp/DIV2K/test/girl_lr.jpg"
+    img_path = "/root/autodl-tmp/DIV2K/test/girl_lr.jpg"    # 测试图片位置，需要自行修改
     img = Image.open(img_path)
     
-    save_path = "/root/autodl-tmp/Models/SuperResolution/srgan"
+    save_path = "/root/autodl-tmp/Models/SuperResolution/srgan"   # 模型权重保存位置，需要自行修改
     checkpoint = torch.load(save_path, map_location='cpu')
 
     model = Generator(scaling_size=2)
